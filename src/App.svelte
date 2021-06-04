9efa9c905c46781f116289035c85dfb9d9733778
Radim Sückr <radim.suckr@gmail.com>
2021-06-04 14:45:16 +0200

    feat: background music

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,46 @@
 <script>
 	import { Router, Link, Route } from "svelte-routing";
+	import BackgroundMusic from "./BackgroundMusic.svelte";
 	import Game from "./Game.svelte";
 	import Home from "./Home.svelte";
 
 	export let url = "";
+
+	let navbarBurgerSelected = false;
 </script>
 
 <Router {url}>
 	<nav class="navbar" role="navigation" aria-label="main navigation">
-		<div class="navbar-brand">
-			<Link to="/" class="navbar-item">
-				Logo
-			</Link>
+		<div class="container">
+			<div class="navbar-brand">
+				<Link to="/" class="navbar-item">Logo</Link>
 
-			<a
-				role="button"
-				class="navbar-burger"
-				aria-label="menu"
-				aria-expanded="false"
-				data-target="navbarBasicExample"
-			>
-				<span aria-hidden="true" />
-				<span aria-hidden="true" />
-				<span aria-hidden="true" />
-			</a>
-		</div>
+				<a
+					on:click={() =>
+						(navbarBurgerSelected = !navbarBurgerSelected)}
+					role="button"
+					class="navbar-burger"
+					class:is-active={navbarBurgerSelected}
+					aria-label="menu"
+					aria-expanded="false"
+					data-target="navbarBasicExample"
+				>
+					<span aria-hidden="true" />
+					<span aria-hidden="true" />
+					<span aria-hidden="true" />
+				</a>
+			</div>
 
-		<div class="navbar-menu">
-			<div class="navbar-start">
-				<Link to="/" class="navbar-item">Home</Link>
+			<div class="navbar-menu" class:is-active={navbarBurgerSelected}>
+				<div class="navbar-start">
+					<Link to="/" class="navbar-item">Home</Link>
 
-				<Link to="game" class="navbar-item">Game</Link>
+					<Link to="game" class="navbar-item">Game</Link>
+
+					<div class="navbar-item">
+						<BackgroundMusic />
+					</div>
+				</div>
 			</div>
 		</div>
 	</nav>
---
+++ src/BackgroundMusic.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { onMount } from "svelte";
+
+	let audioButton;
+	const audio = new Audio("/bensound-theelevatorbossanova.mp3");
+
+	const playText = "Music &#x23F8&#xFE0F";
+	const pauseText = "Music &#x25B6&#xFE0F";
+
+	const toggleButtonText = () => {
+		if (audio.paused) {
+			audioButton.innerHTML = pauseText;
+		} else {
+			audioButton.innerHTML = playText;
+		}
+	};
+
+	const audioToggle = () => {
+		if (audio.paused) {
+			audio.play();
+			toggleButtonText();
+		} else {
+			audio.pause();
+			toggleButtonText();
+		}
+	};
+
+	onMount(() => {
+		audioButton.innerHTML = "Music &#x25B6&#xFE0F";
+		audio.addEventListener("ended", toggleButtonText);
+	});
+</script>
+
+<button
+	class="button is-primary"
+	type="button"
+	bind:this={audioButton}
+	on:click={audioToggle}
+/>
+
+<style>
+	button.button {
+		transition: all 0.25s ease;
+	}
+
+	button.button:hover {
+		transform: scale(1.25);
+	}
+
+	button.button:active {
+		transition: all 0.05s ease;
+		transform: scale(1.1);
+	}
+</style>
